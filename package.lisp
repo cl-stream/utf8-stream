4c6489dabc87433e452cb06f61b739539797e8a6
Thomas de Grivel <thoxdg@gmail.com>
2017-06-17 22:13:11 +0200

    license

--- package.lisp
+++ package.lisp
@@ -1,5 +1,5 @@
 ;;
-;;  fd-stream  -  Unix file descriptor layer for cl-stream
+;;  babel-stream  -  charset encoding/decoding layer for cl-stream
 ;;
 ;;  Copyright 2017 Thomas de Grivel <thoxdg@gmail.com>
 ;;
